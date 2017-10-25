c2f6548c01f72cac2f3e5640b71c93978c408288
Erik Sigurd Young <sigurd@lambda-conspiracy.net>
2017-10-25 16:12:24 -0400

    there apparently is no fix for stoopid

--- wsgi.py
+++ wsgi.py
@@ -209,6 +209,7 @@ def print_ephemeris():
         address = u'Greenwich Observatory: 51\N{DEGREE SIGN} 28\' 38\"N 0\N{DEGREE SIGN} 0\' 0\"'
     else:
         place = 'geocode'
+        requester_ip = request.access_route[0]
         if requester_ip != '127.0.0.1':
             requester_geocode = geocoder.ip(requester_ip, key=GOOGLE_API_KEY)  # this is more accurate for locations,
             latlng = requester_geocode.latlng
